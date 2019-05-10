643c55618236c1a0d446de82d26be3133dbf6eb7
tsingsonqin <tsingson@gmail.com>
2019-05-11 05:40:28 +0800

    fix bug in gin webserver

--- cmd/cli/go-ums-gin-server/main.go
+++ cmd/cli/go-ums-gin-server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/tsingson/go-ums/pkg/web/xgin"
 )
 
@@ -9,5 +12,13 @@ func main() {
 	hs = xgin.NewHttpServer()
 	r := hs.SetupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	_ = r.Run(":8080")
+	server := http.Server{
+		Addr:    ":3001",
+		Handler: r,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
